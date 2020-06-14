c346a6f4afc808e874d89630c10b213fa0e0c06f
roxyvaught <roxanna.vaught@gmail.com>
2020-06-14 15:54:01 -0500

    add comments feature

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Vote } = require("../../models");
+const { User, Post, Vote, } = require("../../models");
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -65,10 +65,32 @@ router.post('/', (req, res) => {
 router.post('/login', (req, res) => {
 // expects {email: 'lernantino@gmail.com', password: 'password1234'}
 User.findOne({
+  attributes: { exclude: ['password'] },
   where: {
-    email: req.body.email
-  }
-  }).then(dbUserData => {
+    id: req.params.id
+  },
+  include: [
+    {
+      model: Post,
+      attributes: ['id', 'title', 'post_url', 'created_at']
+    },
+    // include the Comment model here:
+    {
+      model: Comment,
+      attributes: ['id', 'comment_text', 'created_at'],
+      include: {
+        model: Post,
+        attributes: ['title']
+      }
+    },
+    {
+      model: Post,
+      attributes: ['title'],
+      through: Vote,
+      as: 'voted_posts'
+    }
+  ]
+}).then(dbUserData => {
   if (!dbUserData) {
     res.status(400).json({ message: 'No user with that email address!' });
     return;
